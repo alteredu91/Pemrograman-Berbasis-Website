b5d702522b73ad0be706bd8264df23966091ec1a
alteredu91 <79585685+alteredu91@users.noreply.github.com>
2025-01-17 01:20:46 +0700

    Delete Praktikum 08_4521210076/belajar_laravel directory

--- Praktikum 08_4521210076/belajar_laravel/resources/views/about.blade.php
+++
@@ -1,4 +0,0 @@
-<x-app-layout title="About">
-    <x-slot name="heading">About</x-slot>
-    About content
-</x-app-layout>
--- Praktikum 08_4521210076/belajar_laravel/resources/views/home.blade.php
+++
@@ -1,4 +0,0 @@
-<x-app-layout>
-    <x-slot name="heading">Home</x-slot>
-    Home
-</x-app-layout>

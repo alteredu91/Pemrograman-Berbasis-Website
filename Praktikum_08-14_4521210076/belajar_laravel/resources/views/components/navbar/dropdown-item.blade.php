d6e8b742fc657ba69a588ad10abf21dd2263e295
alteredu91 <79585685+alteredu91@users.noreply.github.com>
2025-01-17 01:42:22 +0700

    Delete Praktikum_08-14_4521210076/belajar_laravel directory

--- Praktikum_08-14_4521210076/belajar_laravel/resources/views/components/navbar/dropdown-item.blade.php
+++
@@ -1,4 +0,0 @@
-<a {{ $attributes }}
-    class="{{ request()->fullurlis($href) ? "bg-zinc'-700 text-white" : "text-zinc-300 hover:bg-zinc-800 hover:text-white" }} rounded-md px-3 py-2 text-sm font-medium ">
-    {{ $slot}}
-</a>
